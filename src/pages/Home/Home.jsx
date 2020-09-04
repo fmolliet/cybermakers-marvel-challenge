14a63e18fc8460342d2103462961f6c4ad9ef707
fmolliet <fmolliet@hotmail.com>
2020-09-04 01:06:30 -0300

    Corrigido bug de width maximo

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './styles.scss';
 
